ac16f4a6b7c8fc7634a8e68f560025b81fba9364
Steve Harris <git@steveharris.me>
2019-09-04 08:18:06 +0100

    proxies fixed for UCLH

--- jupyter-build.sh
+++ jupyter-build.sh
@@ -1,8 +1,3 @@
 # On the command line type 
 # bash build-jupyter.sh
-docker build -t emap-datascience:latest \ 
-    --build-arg HTTP_PROXY \
-    --build-arg HTTPS_PROXY \    
-    --build-arg http_proxy \
-    --build-arg https_proxy \
-    .
+docker build -t emap-datascience:latest --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy .
